ca4eb52fd351c5146c5a8d589905b359ae99b052
JosGoris <jos.goris91@gmail.com>
2016-01-09 23:12:45 +0100

    Finished the exercise

--- main.R
+++ main.R
@@ -7,8 +7,8 @@ library(raster)
 library(rgdal)
 
 #Import the functions
-source('CalcNDVI.R')
-source('ValueReplacement.R')
+source('R/CalcNDVI.R')
+source('R/ValueReplacement.R')
 
 #Download the data
 download.file(url='https://www.dropbox.com/s/i1ylsft80ox6a32/LC81970242014109-SC20141230042441.tar.gz?dl=0', destfile='./data/LandsatNDVI1.tar.gz', method='wget')
@@ -26,5 +26,26 @@ landsat8list <- list.files('data/', pattern = glob2rx('LC8*.tif'), full.names = 
 landsat5 <- stack(landsat5list)
 landsat8 <- stack(landsat8list)
 
+#Extract cloud layers from the stack
+fmask5 <- landsat5[[1]]
+fmask8 <- landsat8[[1]]
+
+#Remove layer from stack
+landsat5_2 <- dropLayer(landsat5,1)
+landsat8_2 <- dropLayer(landsat8,1)
+
+#Replace values
+landsat5cloudf <- overlay(x = landsat5_2, y = fmask5, fun = cloud2NA)
+landsat8cloudf <- overlay(x = landsat8_2, y = fmask8, fun = cloud2NA)
+
+#Calculate NDVI 
+ndvi5 <- overlay(x=landsat5cloudf[[6]], y=landsat5cloudf[[7]], fun=ndvOver)
+ndvi8 <- overlay(x=landsat8cloudf[[4]], y=landsat8cloudf[[5]], fun=ndvOver)
+
+#subtract the images
+diffNDVI <- ndvi5-ndvi8
+
+#plot the result
+plot(diffNDVI)
 
 
